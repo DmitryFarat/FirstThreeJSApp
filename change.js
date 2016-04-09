9afd8b2b06d999434e49d232509b4de97a905569
dima <dimaFart3@gmail.com>
2016-04-09 10:32:10 +0200

    Update:add contact page

--- change.js
+++ change.js
@@ -24,4 +24,6 @@ Update:fix the pastwork and add new work// Auto commit at timestamp: Thu Nov 21 
 console.log('Auto commit updated the JS file');
 Update:change the profile description and add new avatars// Auto commit at timestamp: Thu Nov 21 18:44:16 2024
 console.log('Auto commit updated the JS file');
-Update:add new puzzle games
+Update:add new puzzle games// Auto commit at timestamp: Thu Nov 21 18:44:25 2024
+console.log('Auto commit updated the JS file');
+Update:add contact page
