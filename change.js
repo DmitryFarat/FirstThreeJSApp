0d642ff530e56a1601de9aae4fe3765e2bb3bc29
dima <dimaFart3@gmail.com>
2017-06-10 18:11:48 +0200

    Update:fix the pastwork and add new work

--- change.js
+++ change.js
@@ -2,4 +2,6 @@
 console.log('Auto commit updated the JS file');
 Edit README file// Auto commit at timestamp: Thu Nov 21 18:38:15 2024
 console.log('Auto commit updated the JS file');
-Update:first change in blog page
+Update:first change in blog page// Auto commit at timestamp: Thu Nov 21 18:38:27 2024
+console.log('Auto commit updated the JS file');
+Update:fix the pastwork and add new work
